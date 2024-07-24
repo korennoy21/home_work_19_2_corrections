42b4b8b385717b305f2ca16759b1449a0e2b39c9
korennoy21 <165386492+korennoy21@users.noreply.github.com>
2024-07-24 20:21:11 +0300

    url

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('config.urls'))
-]
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('contact/', views.contact, name='contact'),
-]
+    path('', include('catalog.urls', namespace='catalog'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from django.shortcuts import render
 
